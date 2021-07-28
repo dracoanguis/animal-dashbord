3356ffbe2102cfdf4a402d73f7319c6dd0ccc962
Ethan Arm <=>
2021-07-28 11:04:54 +0200

    finished layout structure

--- dashbord.py
+++ dashbord.py
@@ -3,9 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, Input
 import dash_bootstrap_components as dbc
-from numpy.core.fromnumeric import trace
 import pandas as pd
-from pandas.core.frame import DataFrame
 import plotly.express as px
 import os.path
 import geopandas as gp
@@ -17,7 +15,7 @@ app = dash.Dash(__name__,external_stylesheets=[dbc.themes.CYBORG],meta_tags=[{'n
 
 canton_source ='https://raw.githubusercontent.com/dracoanguis/animal-dashbord/main/Data/Cantons-observation.geojson?token=ANUIZ2E3OVFR2HEPEXI6M5LA7XZJI'
 
-canton_path = os.path.join('Data','CantonsCH.geojson')
+canton_path = os.path.join('Data','deprecated','Cantons-observation.geojson')
 
 with open(canton_path,'r') as cp:
 	cantons = json.load(cp)
@@ -57,6 +55,8 @@ mapbox_style_url = 'mapbox://styles/gargan/ckrgedfau5bc417o52w9owh1g'
 # 	)
 # ]
 
+MAP_COLOR_SCHEME = [[0,'#fff6c'],[0.125,'#f6e035'],[0.25,'#e79522'],[0.375,'#cb2f2b'],[0.5,'#910003'],[0.625,'#5e0000'],[0.75,'#340000'],[0.825,'#a74694']]
+
 datafig = [
 			# dict(
             #     opacity=1,
@@ -76,10 +76,10 @@ datafig = [
 				geojson=cantons,
 				featureidkey='properties.id', # place where the id of a feature is fetched
 				locations=[cantons['features'][x]['properties']['id'] for x in range(26)],
-				z=[cantons['features'][x]['properties']['id'] for x in range(26)],
+				z=[cantons['features'][x]['properties']['NUMPOINTS'] for x in range(26)],
 				showscale=False, #hide the atrocious colorbar legend
 				hovertext=[cantons['features'][x]['properties']['name'] for x in range(26)],
-				colorscale=[[0,'#fcc0c0'],[0.5,'#f14170'],[1,'#b1064e']] # colorscale wich was determined on the other thing			
+				colorscale=MAP_COLOR_SCHEME # colorscale wich was determined on the other thing			
             )
 ]
 
@@ -126,28 +126,175 @@ fig = dict(
 # statfig = px.pie(data_frame=statimal,values='verbatimScientificName',names=statimal.index)
 # statfig.update_layout(paper_bgcolor='#000',font_color='#fff',margin={'r':0,'l':0,'t':0,'b':0})
 
+#Layout general variable
+
+CARD_COLOR = '#353232'
+
 
 #Layout of the page
 #--------------------------------------------------------------------------------------
 app.layout=dbc.Container([
-    dbc.Row([
-        dbc.Col(html.H1('Switzerland by Animals',className='text-center'))
-
-    ]),
-    dbc.Row([
-        dbc.Col(dcc.Graph(id='swiss-animals',figure=fig),width={'size':7,'offset':0,'order':1}),
-        dbc.Col(dcc.Graph(id='animal-stats',figure={}),width={'size':5,'offset':0,'order':2})
-    ],no_gutters=False,justify='start'),
-    dbc.Row([
-
-    ])
-])
+    dbc.Row( #title row
+        dbc.Col(
+			dbc.Card(
+				dbc.CardBody(
+					[
+						html.H5('Fluffboard - human-animal interactions with consideration of population density',
+						className='card-title',
+						),
+						html.H6('Evaluations based on the reporting data from GBIF and iNaturalist',
+						className='card-subtitle'
+						)
+					],
+				),
+				id='title-Card',
+				className='text-white w-150',
+				color=CARD_COLOR,
+				style={'width':'90vw'}
+			),
+			className='m-2 px-5',
+		),
+    ),
+    dbc.Row([ #body row
+        dbc.Col( #Left column
+			[
+				dbc.Row( #Map location
+					dbc.Col(
+						dbc.Card(
+							dcc.Graph(
+								id='swiss-map',
+								figure=fig,
+								className='m-1',
+								style={'height':'50vh'}
+							),
+							color=CARD_COLOR,
+							className='m-1'
+						),
+						width={'size':11,'offset':0}
+					),
+					no_gutters=True,
+					justify='center'
+				),
+				dbc.Row( #Counts location
+					dbc.Col(
+						dbc.Card(
+							'blip blop blorp',
+							className='m-1'
+						),
+						width={'size':11,'offset':0}
+					),
+					no_gutters=True,
+					justify='center'
+				)
+			],
+			width=7,
+			className='align-content-right',
+			style={'margin-left':'0px','margin-right':'0px'},
+			align='start'
+		),
+        dbc.Col( #right column
+			[
+				dbc.Row( #filters location
+					dbc.Col(
+						dbc.Card(
+							[
+								dbc.CardHeader(
+									html.H6('Population selector'),
+									className='text-white'
+								),
+								dbc.CardBody(
+									[
+										dcc.Slider(
+											id='population-slider',
+											step=None,
+											min=0,
+											max=1000,
+											value=0,
+											marks={
+												num:{
+													'label':str(num),
+													'style':{'color':'white'}
+												}
+												for num in [x*100 for x in range(0,11)]
+											}
+										)
+									],
+									className='m-0'
+								)
+							],
+							className='m-1',
+							color=CARD_COLOR,
+							style={'height':'16vh'}
+						),
+						width={'size':11,'offset':0}
+					),
+					no_gutters=True
+				),
+				dbc.Row( #pie chart proportion location
+					dbc.Col(
+						dbc.Card(
+							[
+								dbc.CardHeader(
+									html.H6('Proportion of animals by species',className='text-white')
+								),
+								dbc.CardBody(
+									dcc.Graph(
+										id='animal-proportion',
+										figure={},
+										className='m-1',
+										style={'height':'21vh'}
+									)
+								)
+							],
+							className='m-1',
+							color=CARD_COLOR,
+							style={'height':'32vh'}
+						),
+						width={'size':11,'offset':0}
+					),
+					no_gutters=True,
+					justify='start'
+				),
+				dbc.Row( #Bar chart number proportion
+					dbc.Col(
+						dbc.Card(
+							[
+								dbc.CardHeader(
+									html.H6('Number of animals by species',className='text-white')
+								),
+								dbc.CardBody(
+									dcc.Graph(
+										id='animal-number',
+										figure={},
+										className='m-1',
+										style={'height':'21vh'}
+									)
+								)
+							],
+							className='m-1',
+							style={'height':'32vh'},
+							color=CARD_COLOR
+						),
+						width={'size':11,'offset':0}
+					),
+					no_gutters=True
+				)
+			],
+			width=5,
+			style={'margin-left':'0px','margin-right':'0px'}
+		)
+    ],no_gutters=True,
+	),
+],fluid=True,className='mr-0 ml-0')
 
 #Callback: update
 #---------------------------------------------------------------------------------------
 
 # @app.callback(
-#     Output(component_id='swiss-animals',component_property='figure')
+#     [
+# 		Input(component_id='population-slider',component_property='value')
+# 		Output(component_id='swiss-map',component_property='figure')
+# 	]
 # )
 # def update_graph():
 
